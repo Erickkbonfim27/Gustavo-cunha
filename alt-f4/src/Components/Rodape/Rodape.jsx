9ee0485ebd26e1e90d3c9b8136468ba9a74b66fd
Erickkbonfim <141456725+Erickkbonfim27@users.noreply.github.com>
2024-10-26 22:26:05 -0300

    responsividade

--- alt-f4/src/Components/Rodape/Rodape.jsx
+++ alt-f4/src/Components/Rodape/Rodape.jsx
@@ -1,7 +1,14 @@
+import textura from "../../Imgs/Ruido.png";
+import logo from "../../Imgs/Alt.png";
+
+
 export default function Rodape(){
     return(
-        <div className="Rodape">
-
+        <div className="Rodape-ftalt">
+            <img src={textura} alt="textura" />
+            <div className="logo-box">
+                <img src={logo} alt="logo" />
+            </div>
         </div>
     )
 }
